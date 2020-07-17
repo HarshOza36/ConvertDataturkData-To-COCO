4320b5b6cb9077fd3baa63a97914e88a2b0003ae
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2020-07-17 14:57:31 +0530

    xml to csv added

---
+++ xml_to_csv.py
@@ -0,0 +1,56 @@
+# import os
+# import glob
+# import pandas as pd
+# import xml.etree.ElementTree as ET
+
+
+# def xml_to_csv(path):
+#     xml_list = []
+#     for xml_file in glob.glob(path + '/*.xml'):
+#         tree = ET.parse(xml_file)
+#         root = tree.getroot()
+#         for member in root.findall('object'):
+
+#             # new box x1x2y1y2
+#             value = (root.find('filename').text,
+#                      int(root.find('size')[0].text),
+#                      int(root.find('size')[1].text),
+#                      member[0].text,
+#                      [int(member[5][0].text),
+#                       int(member[5][2].text),
+#                       int(member[5][1].text),
+#                       int(member[5][3].text)],
+#                      int(member[5][0].text),
+#                      int(member[5][1].text),
+#                      int(member[5][2].text),
+#                      int(member[5][3].text)
+#                      )
+#             # old
+#             # value = (root.find('filename').text,
+#             #          int(root.find('size')[0].text),
+#             #          int(root.find('size')[1].text),
+#             #          member[0].text,
+#             #          [int(member[5][0].text),
+#             #           int(member[5][1].text),
+#             #           int(member[5][2].text),
+#             #           int(member[5][3].text)],
+#             #          int(member[5][0].text),
+#             #          int(member[5][1].text),
+#             #          int(member[5][2].text),
+#             #          int(member[5][3].text)
+#             #          )
+#             xml_list.append(value)
+#     column_name = ['filename', 'width', 'height',
+#                    'class', 'bbox', 'xmin', 'ymin', 'xmax', 'ymax']
+#     xml_df = pd.DataFrame(xml_list, columns=column_name)
+#     return xml_df
+
+
+# def main():
+#     image_path = os.path.join(os.getcwd(), 'pascal_voc_op')
+#     xml_df = xml_to_csv(image_path)
+#     xml_df.to_csv('train.csv', index=None)
+#     print('Successfully converted xml to csv.')
+
+
+# main()
