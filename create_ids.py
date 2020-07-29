07df930c44cb31b0ca5fc3155a30e85b316ea2f5
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2020-07-29 13:05:42 +0530

    Update create_ids.py

--- create_ids.py
+++ create_ids.py
@@ -1,6 +1,10 @@
 import os
 dirr = os.listdir('pascal')
 dirr.remove('downloads')
+if('ids.txt' in dirr):
+    dirr.remove('ids.txt')
+if('labels.txt' in dirr):
+    dirr.remove('labels.txt')
 with open('ids.txt', 'a') as f:
     for i in range(len(dirr)):
         f.write(dirr[i]+"\n")
