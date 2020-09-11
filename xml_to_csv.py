72597fa6dcd3b4c2e26655187990576a6ff63373
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2020-09-11 19:00:29 +0530

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -47,7 +47,7 @@ def xml_to_csv(path):
 
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'pascal_voc_op')
+    image_path = os.path.join(os.getcwd(), 'pascal')
     xml_df = xml_to_csv(image_path)
     xml_df.to_csv('train.csv', index=None)
     print('Successfully converted xml to csv.')
